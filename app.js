4b375d42d2f91b1b1c6ea20c15b78168d51aff47
Dorival <dpichamba@est.ups.edu.ec>
2019-11-17 02:53:06 -0500

    Commita Prueba servidor

--- app.js
+++ app.js
@@ -5,30 +5,65 @@ const { leer, Datos } = require('./metodos/lecturacsv.js');
 
 let comando = argv._[0]
 console.log(comando)
+
 switch (comando) {
+
+    //Seccion publicar
     case 'publicar':
         leer(argv.file).then(archivo => {
             Datos(archivo, argv.year, argv.country).then(archivo2 => {
                 const http = require('http'),
-                
-                fs = require('fs');
-                
-                fs.readFile('./index.html', function (err, html) {
+
+                    fs = require('fs');
+
+                ///Opcion de servidores
+                const hostname = 'localhost';
+                const port = '8000';
+
+                function onRequest(solicitud, respuesta) {
+                    respuesta.writeHeader(200, { 'Content-Type': 'text/html' });
+                    fs.readFile('./index.html', null, function(error, data) {
+                        if (error) {
+                            respuesta.writeHead(404);
+                            respuesta.write('Pagina no Encontrada');
+                        } else {
+                            respuesta.write(data);
+                            respuesta.end(archivo2);
+                        }
+                        respuesta.end();
+                    });
+                }
+                const server = http.createServer(onRequest);
+                server.listen(port, hostname, () => {
+                    console.log(`Servidor corriendo en http://${hostname}:${port}`);
+                })
+
+                ////
+                fs.readFile('./index.html', function(err, html) {
                     if (err) {
-                        throw err; 
-                    }       
-                    http.createServer(function(request, response) {  
-                        response.writeHeader(200, {"Content-Type": "text/html"});  
-                        response.write(html);  
-                        response.end(archivo2);  
+                        throw err;
+                    }
+                    const server = http.createServer(function(request, response) {
+                        response.statusCode = 200;
+                        response.writeHeader(200, { "Content-Type": "text/html" });
+                        response.write(html);
+                        response.end(archivo2);
                     }).listen(3000);
+
+                    // server.listen(port,hostname,()=>{
+                    //   console.log(`Server running at http://${hostname}:${port}/`);
+                    //})
                     console.log('El servidor esta funcionando correctamente');
                 });
 
 
             }).catch((err) => console.log("error: ", err.red))
         }).catch((err) => console.log("error: ", err.red))
+
+
         break;
+
+        //Seccion Guardar 
     case 'guardar':
         leer(argv.file).then(archivo => {
             Datos(archivo, argv.year, argv.country).then(archivo2 => {
